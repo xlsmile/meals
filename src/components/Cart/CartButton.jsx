de0b3b4465c975bfc1349442125ff22b2fa47d5c
Serge Seva <sergeseva@catalyst.net.nz>
2022-11-28 11:11:08 +1300

    Add cart button animation.

--- src/components/Cart/CartButton.jsx
+++ src/components/Cart/CartButton.jsx
@@ -1,14 +1,38 @@
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import CartContext from "../Store/CartContext";
 
+
 const CartButton = ({showCartModal}) => {
+
+  const [btnIsHighlighted, setbtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+
+  const {items} = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${btnIsHighlighted ? "cta cta--cart bump" : "cta cta--cart"}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setbtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setbtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+
+  }, [items]);
+
   return (
-    <button className="cta cta--cart" onClick={showCartModal}>
+    <button className={btnClasses} onClick={showCartModal}>
       <i className="fa-solid fa-shopping-cart"></i>
         Cart
       <span className="counter__cart">{numberOfCartItems}</span>
